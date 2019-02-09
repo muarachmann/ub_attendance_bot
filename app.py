3a4d30a5de576847c63928333ad7e168828d514c
muarachmann <muarachmann@gmail.com>
2019-02-09 11:27:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ def check():
             
             if (event_type == "team_join"):
                 user = events_data["event"]["user"]
-                welcome_text = "Welcome to Cameroon GSoCers Workspace. \nTell us a little bit about yourself, Your Real Names, Location, \nSpecialty/Department, Your Interests, Any specific questions ? \nLearn Netiquette Rules on http://www.albion.com/netiquette/corerules.html \nLearn How To Ask Smart Questions on http://www.catb.org/esr/faqs/smart-questions.html \nRemember to put your Real Names on your profile and a professional profile picture :) \nTake note of the channel topic and ALL pinned posts"
+                welcome_text = "Welcome to Attendance record platform"
                 sc.api_call(
                     "chat.postMessage",
                     channel=general_channel,
-                    text="Hi @" + user["profile"]["display_name"] + ", \n" + welcome_template
+                    text="Hi <@" + user + ">, \n" + welcome_text
                 )
                 return make_response("", 200)
 
